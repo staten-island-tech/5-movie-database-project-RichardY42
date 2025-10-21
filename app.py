37c84bc1832a8c557570bd443a15b1a28f7ae21a
RichardY42 <RichardY42@nycstudents.net>
2025-10-21 10:15:02 -0400

    step 2

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-dataamo=len(data)
-for t in dataamo:
-    #if t=="title":
-    print(data[t]["title"])
 
-#print(len(data))
+"""for i in range(len(data)):
+    print(data[i]['year'])
+
+year=input("give me a year I will print all movies after that year: ")
+for i in range(len(data)):
+    if int(data['year']) > int(year):
+        print(data['year'])"""
+print(data['year'])
---
+++ test.py
@@ -0,0 +1,8 @@
+import json
+## Open the JSON file of movie data
+movies = open("./movies.json", encoding="utf8")
+## create variable "data" that represents the enitre movie list
+data = json.load(movies)
+
+for i in range(len(data)):
+    print(data[i]['year'])
