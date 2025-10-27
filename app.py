de45ebb6f1abba2126c5fef14226b75923bdd579
RichardY42 <RichardY42@nycstudents.net>
2025-10-27 10:15:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
-"""for i in range(len(data)):
+""" for i in range(len(data)):
     print(data[i]['year'])
 
-year=input("give me a year I will print all movies after that year: ")
+year=input("give me a year I will print all movies that released after that year: ")
 for i in range(len(data)):
-    if int(data['year']) > int(year):
-        print(data['year'])"""
-print(data['year'])
+    if data[i]['year'] > int(year):
+        print(data[i]['title']) """
+
+afteryear=input("give me a year I will print all movies that released after that year: ")
+beforeyear=input("give me a another year i'll print all the moives before it: ")
+for i in range(len(data)):
+    if data[i]['year'] > int(afteryear):
+        print(data[i]['title'])
+    if data[i]['year'] < int(afteryear):
+            print(data[i]['title'])
